48253e636321e5802094b4c08244711458e0b1e4
seversait <120575725+seversait@users.noreply.github.com>
2023-04-08 14:40:21 +0300

    Add files via upload

---
+++ 10-5/screenSize-10-5.js
@@ -0,0 +1,30 @@
+const btn = document.querySelector('.j-btn-test');
+//Размер экрана
+const screenWidth = window.screen.width;
+const screenHeight = window.screen.height;
+
+//Размер окна браузера с учётом полосы прокрутки
+const windowInnerWidthScroll = window.innerWidth;
+const windowInnerHeightScroll = window.innerHeight;
+
+//Размер окна браузера без учёта полосы прокрутки
+const windowInnerWidth = document.documentElement.clientWidth;
+const windowInnerHeight = document.documentElement.clientHeight;
+
+//Размер веб-страницы
+const pageWidth = document.documentElement.scrollWidth;
+const pageHeight = document.documentElement.scrollHeight;
+
+
+
+btn.addEventListener('click', () => {
+
+alert(`Размер экрана: ${screenWidth}px x ${screenHeight}px. 
+Размер окна браузера с полосой прокрутки: ${windowInnerWidthScroll}px x ${windowInnerHeightScroll}px.
+Размер окна браузера без полосы прокрутки: ${windowInnerWidth}px x ${windowInnerHeight}px.
+Размер веб-страницы: ${pageWidth}px x ${pageHeight}px.`);
+
+});
+
+
+
